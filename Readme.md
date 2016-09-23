492488a0e568f672cdc3ca9f7eebcb2abe195485
nowordforfree <nowordforfree@gmail.com>
2016-09-23 18:55:35 +0300

    Working on UI layout

---
+++ Readme.md
@@ -0,0 +1,8 @@
+###This is a  media player based on electron and VLC binding to nodejs/electron
+
+It should support playback of variety audio and video formats, but at playing video is not working yet.
+
+Reminder list:
+[ ] - fix search in playlists (make magnifier icon instead of button)
+[ ] - add missing buttons and bind them to player methods (prev, stop, next)
+[ ] - fix dropdown for adding files (update package when will be fixed dropdown/dropup [github issue 4762](https://github.com/angular-ui/bootstrap/issues/4762))
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
+    imports: [ BrowserModule, Ng2BootstrapModule ],
     declarations: [
         AppComponent,
         PlayerComponent,
         PlaylistComponent
     ],
-    imports: [ BrowserModule ]
+    bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
